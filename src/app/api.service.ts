e06f93b1c03dd320d1e014da1a17695a774ca4d4
toua moua <touam@msn.com>
2024-08-13 10:34:34 -0600

    added api service

---
+++ src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private baseUrl = 'https://swapi.dev/api/';
+
+  constructor(private http: HttpClient) { }
+
+  public getPeople() {
+    const url = `${this.baseUrl}/people/`;
+    return this.http.get(url).pipe(map((resp: any) => resp.results));
+  }
+}
