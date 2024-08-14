56bc257c3398e966ebf7bc1f2f2f3af05587dcfb
toua moua <touam@msn.com>
2024-08-14 10:39:08 -0600

    added error handling to api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs';
+import { catchError, map, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +12,23 @@ export class ApiService {
 
   public getPeople() {
     const url = `${this.baseUrl}/people/`;
-    return this.http.get(url).pipe(map((resp: any) => resp.results));
+    return this.http.get(url).pipe(
+      map((resp: any) => resp.results),
+      catchError((err: any) => {
+        console.log('caught people error and rethrowing', err);
+        return throwError(() => err);
+      })
+    );
   }
 
   public getStarships() {
     const url = `${this.baseUrl}/starships/`;
-    return this.http.get(url).pipe(map((resp: any) => resp.results));
+    return this.http.get(url).pipe(
+      map((resp: any) => resp.results),
+      catchError((err: any) => {
+        console.log('caught starships error and rethrowing', err);
+        return throwError(() => err);
+      })
+    );
   }
 }
