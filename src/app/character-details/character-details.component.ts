d05ea4d330957dd1036104c1fc532d89e4942c5b
toua moua <touam@msn.com>
2024-11-14 18:26:29 -0700

    refactored character details view

--- src/app/character-details/character-details.component.ts
+++ src/app/character-details/character-details.component.ts
@@ -3,11 +3,12 @@ import { Observable, tap } from 'rxjs';
 import { ApiService } from '../api.service';
 import { ActivatedRoute } from '@angular/router';
 import { AsyncPipe, KeyValuePipe } from '@angular/common';
+import { LoaderComponent } from '../loader/loader.component';
 
 @Component({
   selector: 'app-character-details',
   standalone: true,
-  imports: [AsyncPipe, KeyValuePipe],
+  imports: [AsyncPipe, KeyValuePipe, LoaderComponent],
   templateUrl: './character-details.component.html',
   styleUrl: './character-details.component.less'
 })
@@ -20,6 +21,7 @@ export class CharacterDetailsComponent {
   ngOnInit() {
     // Since character details page is only created once, subscribe to route params to detech url change to get id.
     this.route.paramMap.subscribe((params) => {
+      this.isLoading = true;
       // Get character id from url. Id is set in the character component view.
       this.person$ = this.api.getPersonById(params.get('id') ?? "0").pipe(tap(() => this.isLoading = false));
     })
